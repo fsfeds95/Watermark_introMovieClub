efb21b99c545a8b698be681fc40b3fddd7a6b8a3
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-01 22:57:07 -0400

    Actualizar app.js

--- app.js
+++ app.js
@@ -109,8 +109,8 @@ app.get('/b', async (req, res) => {
   watermark2.resize(1280, 720);
 
   // Establece la opacidad de la watermark1 a 0.375 y watermark2 a 0.75
-  watermark1.opacity(0.08);
-  watermark2.opacity(0.40);
+  watermark1.opacity(0.16);
+  watermark2.opacity(0.50);
 
   // Combinar las marcas de agua en una sola imagen
   watermark1.composite(watermark2, 0, 0, {
